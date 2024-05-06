7fc97b077f11d50a3196fafaaba0472b062776a2
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-05-06 16:17:55 +0300

    Injector constructor are fixed

--- src/main/java/org/example/Injector.java
+++ src/main/java/org/example/Injector.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Injector {
     private Properties properties;
 
-    public Injector(Properties properties) {
-        this.properties = properties;
+    public Injector(String filePath) {
+        this.properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            System.err.println("Error loading properties file: " + e.getMessage());
+        }
     }
 }
