e4986bd540abfefb86cab3d863c97cb95d1c1ef7
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-05-06 16:09:50 +0300

    Injector constructor are implemented

--- src/main/java/org/example/Injector.java
+++ src/main/java/org/example/Injector.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.Properties;
+
 public class Injector {
+    private Properties properties;
+
+    public Injector(Properties properties) {
+        this.properties = properties;
+    }
 }
