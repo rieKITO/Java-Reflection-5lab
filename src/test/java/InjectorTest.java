932cbacc0dc49038c7330dbaeb86430fea4ac7f9
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-05-06 16:40:01 +0300

    added inject test

--- src/test/java/InjectorTest.java
+++ src/test/java/InjectorTest.java
@@ -1,2 +1,19 @@
+import org.example.Injector;
+import org.example.SomeBean;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+
 public class InjectorTest {
+    @Test
+    public void testInject() throws Exception {
+        String filePath = "propertiesFile.properties";
+        Injector injector = new Injector(filePath);
+
+        SomeBean sb = injector.inject(new SomeBean());
+
+        assertNotNull(sb);
+        assertNotNull(sb.getField1());
+        assertNotNull(sb.getField2());
+    }
 }
