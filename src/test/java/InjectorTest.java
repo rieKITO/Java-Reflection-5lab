39e8510ef6e6e38c9a753f4ec687499720be4cd0
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-05-06 16:52:36 +0300

    added inject test with no properties

--- src/test/java/InjectorTest.java
+++ src/test/java/InjectorTest.java
@@ -2,7 +2,10 @@ import org.example.Injector;
 import org.example.SomeBean;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertThrows;
 
 public class InjectorTest {
     @Test
@@ -15,5 +18,13 @@ public class InjectorTest {
         assertNotNull(sb);
         assertNotNull(sb.getField1());
         assertNotNull(sb.getField2());
+    }
+
+    @Test
+    public void testInjectNoProperties() {
+        String filePath = "nonexistent.properties";
+        Injector injector = new Injector(filePath);
+
+        assertThrows(RuntimeException.class, () -> injector.inject(new SomeBean()));
     }
 }
